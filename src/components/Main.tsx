6303f3c0f9e0e431340f768cfdd7aeac3094d63f
Arman Sakif <55103606+arman-sakif@users.noreply.github.com>
2025-05-23 17:47:28 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import avatar from '../assets/images/avatar.png';
 
 function Main() {
 
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={avatar} alt="My Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
